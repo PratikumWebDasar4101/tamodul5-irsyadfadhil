e927d0d6af8ab35144888a8e7f25197d54e691a3
Chad <42725274+irsyadfadhil@users.noreply.github.com>
2018-10-02 22:48:03 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,18 @@
-<?php
-
-
-
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FormLogin</title>
+</head>
+<body>
+	<center><h1>LOGIN</h1></center>
+	<hr>
+	<center>
+	<form action="prosesLogin.php" method="POST">
+	Username :<input type="text" name="username" placeholder="Masukkan Username anda"><br><br>
+	Password :<input type="password" name="password" placeholder="Masukkan Password anda"><br><br>
+	<input type="submit" name="submit" value="Login">
+	</form>
+	</center>
+	<hr>
+</body>
+</html>
